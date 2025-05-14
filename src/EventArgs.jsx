7090b7a7b16a57096990ce9f02c6ded186131b39
chihaya-425 <chihamaro.ta@gmail.com>
2025-05-14 15:15:41 +0900

    Todoリスト 追加機能の作製

--- src/EventArgs.jsx
+++ src/EventArgs.jsx
@@ -1,40 +1,38 @@
 import React from "react";
 import{useState} from 'react';
+//サンプルコードをテストしたい場合はexportから下を変更する
+let maxId = 0;
+export default function StateTodo(){
+  const [title, setTitle]=useState('');
+  const [todo, setTodo] =useState([]);
 
-export default function FormCheckedMulti(){
-  const [ form , setForm] = useState({
-    animal : ['dog','hamstar']
-  });
-
-  const handleFormMulti = e => {
-    const fa = form.animal;
-    if (e.target.checked){
-      fa.push(e.target.value);
-    }else{
-      fa.splice(fa.indexOf(e.target.value),1);
-    }
-    setForm({
-      ...form,
-      [e.target.name]:fa
-    });
+  const handleChangeTitle = e =>{
+    setTitle(e.target.value);
   };
-  const show = () =>{
-    console.log(`好きな動物：${form.animal}`);
-  } ;
-  return (
-    <form>
-      <fieldset>
-      <legend>好きな動物</legend>
-      <label htmlFor = 'animal_dog'>イヌ</label>
-      <input id = 'animal_dog' name = 'animal' type = 'checkbox' value = 'dog' checked = {form.animal.includes('dog')}onChange={handleFormMulti}/><br />
-      <label htmlFor = 'animal_dog'>ネコ</label>
-      <input id = 'animal_cat' name = 'animal' type = 'checkbox' value = 'cat' checked = {form.animal.includes('cat')}onChange={handleFormMulti}/><br />
-      <label htmlFor = 'animal_dog'>ハムスター</label>
-      <input id = 'animal_hamstar' name = 'animal' type = 'checkbox' value = 'hamstar' checked = {form.animal.includes('hamstar')}onChange={handleFormMulti}/><br />
-      <label htmlFor = 'animal_dog'>ウサギ</label>
-      <input id = 'animal_rabbit' name = 'animal' type = 'checkbox' value = 'rabbit' checked = {form.animal.includes('rabbit')}onChange={handleFormMulti}/><br />
-      </fieldset>
-      <button type = 'button' onClick={show}>送信</button>
-    </form>
-  );
-}
+
+  const handleClick = () => {
+    setTodo([
+      ...todo,
+      {
+        id : ++maxId,
+        title,
+        created : new Date(),
+        isDone : false
+      }
+    ]);
+  };
+  return(
+    <div>
+     <label>
+      やること：<input type="text" name="title" value={title} onChange={handleChangeTitle}/>
+     </label>
+     <button type="button" onClick={handleClick}>追加</button>
+     <hr />
+     <ul>
+      {todo.map(item =>(
+        <li key={item.id}>{item.title}</li>
+      ))}
+     </ul>
+    </div>
+  ); 
+ }
